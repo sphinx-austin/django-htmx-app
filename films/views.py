c4319e6df538d69434603504522983e04b97b939
sphinx-austin <austinjuniour4@gmail.com>
2023-03-23 16:50:38 +0300

    add views

--- films/views.py
+++ films/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth import get_user_model
 
 from films.forms import RegisterForm
 
+from films.models import Film
+from django.views.generic.list import ListView
+
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -23,9 +26,36 @@ class RegisterView(FormView):
         form.save()  # save the user
         return super().form_valid(form)
     
+
+class FilmList(ListView):
+    template_name = 'films.html'
+    model = Film
+    context_object_name = 'films'
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.films.all()  
+    
 def check_username(request):
     username = request.POST.get('username')
     if get_user_model().objects.filter(username=username).exists():
         return HttpResponse("<div id='username-error' class='error'>This username already exists</div>")
     else:
         return HttpResponse("<div id='username-error' class='success' >This username is available</div>")
+
+
+
+def add_film(request):
+    name = request.POST.get('filmname')
+
+    film = Film.objects.create(name=name)
+
+    # add the film to the user's list
+    request.user.films.add(film)
+
+
+    # return template will all of the user's films
+    films = request.user.films.all()
+    return render(request, 'partials/film-list.html', {'films':films})
+
+
