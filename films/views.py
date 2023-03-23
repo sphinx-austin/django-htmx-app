21172da5fa8dd8ee09d350a0f1c77ef6f5cd6d11
sphinx-austin <austinjuniour4@gmail.com>
2023-03-23 12:17:01 +0300

    register htmx url pattern view

--- films/views.py
+++ films/views.py
@@ -22,3 +22,10 @@ class RegisterView(FormView):
     def form_valid(self, form):
         form.save()  # save the user
         return super().form_valid(form)
+    
+def check_username(request):
+    username = request.POST.get('username')
+    if get_user_model().objects.filter(username=username).exists():
+        return HttpResponse("<div style='color: red'>This username already exists</div>")
+    else:
+        return HttpResponse("<div style='color: green'>This username is available</div>")
